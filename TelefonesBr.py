d8b456d293ba8b3b718fb1a5ef20f871085aacd6
Kayke Alves <alveskayke033@gmail.com>
2021-07-21 17:28:26 -0300

    validação para telefones br

---
+++ TelefonesBr.py
@@ -0,0 +1,30 @@
+#importando a lib regular expressions(expressões regulares)
+import re
+
+class TelefonesBr:
+    def __init__(self, telefone):
+        if self.valida_telefone(telefone):
+            self.numero = telefone
+        else:
+            raise ValueError("Número de telefone incorreto!")
+
+    def valida_telefone(self, telefone):
+        padrao = "([0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.findall(padrao, telefone)
+        if resposta:
+            return True
+        else:
+            return False
+
+    def format_numero(self):
+        padrao_telefone = "([0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao_telefone, self.numero)
+        numero_formatado = "+{}({}){}-{}".format(
+            resposta.group(1),
+            resposta.group(2),
+            resposta.group(3),
+            resposta.group(4),)
+        return numero_formatado
+
+    def __str__(self):
+        return self.format_numero()
