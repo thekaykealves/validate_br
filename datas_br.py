8262dc518e68eb450abe957f64be435b6c855568
Kayke Alves <alveskayke033@gmail.com>
2021-07-23 17:57:18 -0300

    formatação de datas e momento de cadastro

---
+++ datas_br.py
@@ -0,0 +1,33 @@
+from datetime import datetime,timedelta
+
+class Datas_Br:
+    def __init__(self):
+        self.momento_cadastro = datetime.today()
+
+    def __str__(self):
+        return self.format_data()
+
+    def mes_cadastro(self):
+        meses_do_ano = [
+            "janeiro", "fevereiro", "março", "abril",
+            "maio", "junho", "julho", "agosto", "setembro",
+            "outubro", "novembro", "dezembro"
+        ]
+        mes_cadastro = self.momento_cadastro.month -1
+        return meses_do_ano[mes_cadastro]
+
+    def dia_semana(self):
+        lista_dias_semana= [
+            "segunda", "terça", "quarta",
+            "quinta", "sexta", "sabádo", "domingo"
+        ]
+        dia_semana = self.momento_cadastro.weekday()
+        return lista_dias_semana[dia_semana]
+
+    def format_data(self):
+        data_formatada = self.momento_cadastro.strftime("%d/%m/%Y %H:%M")
+        return data_formatada
+
+    def tempo_cadastro(self):
+        tempo_cadastro = (datetime.today() + timedelta(days=30)) - self.momento_cadastro
+        return tempo_cadastro
